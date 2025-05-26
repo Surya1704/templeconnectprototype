a9d537f3cf9e4132e09ff769f752ea51ac6cf97b
Surya1704 <86611204+Surya1704@users.noreply.github.com>
2025-05-26 16:48:32 +0530

    Update extendedTemples.ts

--- src/data/extendedTemples.ts
+++ src/data/extendedTemples.ts
@@ -9,8 +9,7 @@ export const extendedTemples: Temple[] = [
     location: "Kanipakam, Andhra Pradesh",
     state: "Andhra Pradesh",
     rating: 4.89,
-    image: "https://imgs.search.brave.com/t_mmqdGQvmGMiBXAEhScIb_-Jl7kAjRmE8FIUPWApRs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vZERVVlVI/dHpEaWl0Z1lXVndy/QllKbVZaYlBzeVlt/ZWszbXhTb3lIOVFJ/VS9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/TG5CcC9ibWx0Wnk1/amIyMHZiM0pwL1oy/bHVZV3h6THpNekx6/QTQvTHpWakx6TXpN/RGcxWXpOaS9Oak5r/WkRVNFltWTBZVEJs/L016SmlNamd4TXpF/Mk4yTXovTG1wd1p3",
-    hours: "Open 5:00 AM - 9:00 PM",
+    
     price: 50,
     tags: ["Ganesh", "Swayambhu", "Darshan"],
     description: "Kanipakam Vinayaka Temple is a Hindu temple for Lord Ganesha in the village of Kanipakam, Andhra Pradesh. The main deity in the temple, Lord Ganesha, is known to be swayambhu (self-manifested)."
